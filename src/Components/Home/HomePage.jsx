fc049d142c0e948d6981dd6a29a70da76cddbea5
kandakatla saikiran <saikiran3ksk@gmail.com>
2026-01-19 12:57:35 +0530

    Initial commit

---
+++ src/Components/Home/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import iphone from "../../Assets/iphone-14-pro.webp";
+import mac from "../../Assets/mac-system-cut.jfif";
+import HeroSection from "./HeroSection";
+import FeaturedProducts from "./FeaturedProducts";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection
+        title="Buy iPhone 14 Pro"
+        subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
+        link="/product/696730c8db020d346ae3b797"
+        image={iphone}
+      />
+
+      <FeaturedProducts />
+
+      <HeroSection
+        title="Build the ultimate setup"
+        subtitle="you can add studio display to your Mac for a perfect pairing."
+        link="/product/696730c8db020d346ae3b79f"
+        image={mac}
+      />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Components/Navbar/LinkWithIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./LinkWithIcon.css";
+import Rocket from "../../Assets/rocket.png";
+import { NavLink } from "react-router-dom";
+
+const LinkWithIcon = ({ title, link, emoji, sidebar }) => {
+  return (
+    <NavLink
+      to={link}
+      className={sidebar ? "align_center sidebar_link" : "align_center"}
+    >
+      {title} <img src={emoji} alt="" className="link_emoji" />{" "}
+    </NavLink>
+  );
+};
+
+export default LinkWithIcon;
